5919d3f93d2f0d88edc03372c46c7a10dcebb9e3
= <=>
2024-10-10 17:44:25 +0700

    Push Exam 6

---
+++ exam6.js
@@ -0,0 +1,16 @@
+// Chu Vi
+function chuvi(dai, rong) {
+    return chuvi = (dai + rong) * 2;
+}
+// Dien tich
+function dientich(dai, rong) {
+    return dientich = (dai * rong);
+}
+//
+let dai = 4;
+let rong = 2;
+let tinhchuvi = chuvi(dai, rong);
+let tinhdientich = dientich(dai, rong);
+//
+console.log("Chu vi HCN la : " + tinhchuvi);
+console.log("Dien tich HCN la : " + tinhdientich);
