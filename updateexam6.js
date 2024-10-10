907bada20c4a9d76a7eab9a3f625a0a63e59c9f9
= <=>
2024-10-10 18:28:02 +0700

    Updateex6

---
+++ updateexam6.js
@@ -0,0 +1,10 @@
+//Chu vi
+const chuVi = (dai, rong) => (dai + rong) * 2;
+// Dien Tich
+const dienTich = (dai, rong) => dai * rong;
+
+let dai = 4;
+let rong = 2;
+
+console.log("Chu Vi: " + chuVi(dai, rong));
+console.log("Dien Tich: " + dienTich(dai, rong));
